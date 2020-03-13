d55fb6a728629a1ac9ecfe7189ed5777ef6a7e5d
Cole Futch <jcfutch@gmail.com>
2020-03-12 21:25:58 -0400

    Correction to UIKit3Preset.php.

--- src/uikit3-stubs/webpack.mix.js
+++ src/uikit3-stubs/webpack.mix.js
@@ -1,7 +1,5 @@
 const mix = require('laravel-mix');
 
-require('laravel-mix-purgecss');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -15,9 +13,3 @@ require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/scss/app.scss', 'public/css');
-
-if (mix.inProduction()) {
-  mix
-   .version()
-   .purgeCss();
-}
