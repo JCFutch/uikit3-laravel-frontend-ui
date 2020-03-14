15105851f9b673d989bf3295df9f83538d98efdb
Cole Futch <jcfutch@gmail.com>
2020-03-14 10:22:10 -0400

    updated welcome.blade.php

--- src/uikit3-stubs/resources/views/welcome.blade.php
+++ src/uikit3-stubs/resources/views/welcome.blade.php
@@ -8,55 +8,114 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel with UIKit3') }}</title>
 
-    <!-- Styles -->
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <style>
+    .wrap::before {
+    				position: absolute;
+    				height: 100vh;
+    				width: 100vw;
+    				content: '';
+    				z-index: 1;
+    				background-color: rgba(0,0,0,0.6);
+    			}
+    			.lead {
+    				font-size: 1.175em;
+    				font-weight: 300;
+    			}
+    			.uk-logo img {
+    				height: 45px;
+    			}
+
+     .laravel-name {
+       font-family: 'Nunito', sans-serif !important;
+       font-size: 84px;
+       font-weight: 200;
+     }
+
+     .links {
+       padding: 0 25px;
+       font-size: 13px;
+       font-weight: 600;
+       letter-spacing: .1rem;
+       text-decoration: none;
+       text-transform: uppercase;
+     }
+     </style>
+
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="">
-<div class="">
-    @if(Route::has('login'))
-        <div class="">
-            @auth
-                <a href="{{ url('/home') }}" class="">{{ __('Home') }}</a>
-            @else
-                <a href="{{ route('login') }}" class="">{{ __('Login') }}</a>
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="">{{ __('Register') }}</a>
-                @endif
-            @endauth
-        </div>
-    @endif
+<body class="uk-light wrap uk-background-norepeat uk-background-cover uk-background-center-center uk-cover-container uk-background-secondary">
+  <img data-srcset="https://picsum.photos/640/700/?image=251 640w,
+		             https://picsum.photos/960/700/?image=251 960w,
+		             https://picsum.photos/1200/900/?image=251 1200w,
+		             https://picsum.photos/2000/1000/?image=251 2000w"
+		     sizes="100vw"
+		     data-src="https://picsum.photos/1200/900/?image=251" alt="" data-uk-cover data-uk-img>
+  <div class="uk-flex uk-flex-center uk-flex-middle uk-height-viewport uk-position-z-index uk-position-relative" data-uk-height-viewport="min-height: 400">
 
-    <div class="">
-        <div class="">
-            <div>
-                <h1 class="">
-                    {{ config('app.name', 'Laravel') }}
-                </h1>
-                <ul class="list-reset">
-                    <li class="inline pr-8">
-                        <a href="https://laravel.com/docs" class="" title="Documentation">Documentation</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://laracasts.com" class="" title="Laracasts">Laracasts</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://laravel-news.com" class="" title="News">News</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://nova.laravel.com" class="" title="Nova">Nova</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://forge.laravel.com" class="" title="Forge">Forge</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://github.com/laravel/laravel" class="" title="GitHub">GitHub</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <!-- NAV -->
+   			<div class="uk-position-top">
+   				<div class="uk-container uk-container-small">
+   					<nav class="uk-navbar-container uk-navbar-transparent uk-width-expand" data-uk-navbar>
+   						<div class="uk-navbar-left">
+   							<div class="uk-navbar-item">
+   								<a class="uk-logo" href=""><img src="{{ asset('images/uikit-logo.png') }}" alt="Logo"></a>
+   							</div>
+   						</div>
+   						<div class="uk-navbar-right">
+                @if(Route::has('login'))
+   							<ul class="uk-navbar-nav">
+                  @auth
+   								<li class="uk-active uk-visible@m"><a href="{{ url('home') }}" data-uk-icon="home">{{ __('Home') }}</a></li>
+                  @else
+   								<li class="uk-visible@s"><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                  @if (Route::has('register'))
+   								<li class="uk-visible@s"><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                  @endif
+                  @endauth
+   							</ul>
+   						</div>
+              @endif
+   					</nav>
+   				</div>
+   			</div>
+   			<!-- /NAV -->
+
+        <!-- TEXT -->
+<div class="uk-container uk-flex-auto uk-text-center" data-uk-scrollspy="target: > .animate; cls: uk-animation-slide-bottom-small uk-invisible; delay: 300">
+  <h1 class="uk-heading-primary animate uk-invisible laravel-name">{{ config('app.name', 'Laravel') }}</h1>
+
+  <div class="uk-width-4-5@m uk-margin-auto animate uk-invisible">
+					<p class="lead">Laravel Framework with UIKit.</p>
+	</div>
+
+  <div class="uk-margin-medium-top animate uk-invisible uk-flex-inline" data-uk-margin data-uk-scrollspy-class="uk-animation-fade uk-invisible">
+    <ul class="uk-subnav links" uk-margin>
+    <li><a href="https://laravel.com/docs" title="Documentation">Documentation</a></li>
+    <li><a href="https://laracasts.com" title="Laracasts">Laracasts</a></li>
+    <li><a href="https://laravel-news.com" title="News">News</a></li>
+    <li><a href="https://nova.laravel.com" title="Nova">Nova</a></li>
+    <li><a href="https://forge.laravel.com" title="Forge">Forge</a></li>
+    <li><a href="https://github.com/laravel/laravel" title="Github">Github</a></li>
+  </ul>
+  </div>
 </div>
+<!-- /TEXT -->
+
+<!-- FOOT -->
+<div class="uk-position-bottom-center uk-position-small">
+  <span class="uk-text-small uk-text-center">© 2020 | <a href="https://gitlab.turkeycreeknetworks.com/cartographr" target=_blank title="Created by Cartographr">Created by Cartographr</a> | Built with <a href="http://getuikit.com" title="Visit UIkit 3 site" target="_blank" data-uk-tooltip><span data-uk-icon="uikit"></span></a></span>
+</div>
+<!-- /FOOT -->
+
+</div>
+
+<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+
 </body>
 </html>
